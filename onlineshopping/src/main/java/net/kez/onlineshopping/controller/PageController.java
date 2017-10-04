3907623792696961b1aa8c33a5b90d027206726f
azad <akazad.it01@gamil.com>
2017-10-05 00:36:13 +0600

    Success

--- onlineshopping/src/main/java/net/kez/onlineshopping/controller/PageController.java
+++ onlineshopping/src/main/java/net/kez/onlineshopping/controller/PageController.java
@@ -7,31 +7,38 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class PageController {
 
+	@RequestMapping(value = { "/", "/home", "/index" })
+	public ModelAndView index() {
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("title", "Home");
+		mv.addObject("userClickHome", true);
+		return mv;
+	}
+
+	@RequestMapping(value = "/about")
+	public ModelAndView about() {
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("title", "About Us");
+		mv.addObject("userClickAbout", true);
+		return mv;
+	}
+
+	@RequestMapping(value = "/contact")
+	public ModelAndView contact() {
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("title", "Contact Us");
+		mv.addObject("userClickContact", true);
+		return mv;
+	}
+	
+	
+	
 	/* @RequestMapping(value = { "/", "/home", "/index" }) */
 	/*
 	 * @RequestMapping("/") public ModelAndView index() { ModelAndView mv = new
 	 * ModelAndView("page"); mv.addObject("greeting", "Welcome to spring MVC");
 	 * return mv; }
 	 */
-
-	@RequestMapping("/")
-	public ModelAndView root() {
-		System.out.println("root page @@@");
-		return new ModelAndView("page");
-
-	}
-
-	@RequestMapping("/home")
-	public ModelAndView home() {
-		return new ModelAndView("page");
-
-	}
-
-	@RequestMapping("/index")
-	public ModelAndView index() {
-		return new ModelAndView("page");
-
-	}
 
 	/*
 	 * @RequestMapping(value = "/test") public ModelAndView
---
+++ onlineshopping/src/main/webapp/WEB-INF/views/shared/footer.jsp
@@ -0,0 +1,16 @@
+
+<div class="container footer">
+
+	<hr>
+
+	<!-- Footer -->
+	<footer>
+		<div class="row">
+			<div class="col-lg-12">
+				<p>Online Shopping 2017 - Designed and Developed By @@@ A K Azad
+					@@@</p>
+			</div>
+		</div>
+	</footer>
+
+</div>
